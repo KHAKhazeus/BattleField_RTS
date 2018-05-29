86efd68dbd87ae1e055289e702d2199d3b253a22
KHAKhazeus <khazeus@outlook.com>
2018-05-29 09:02:49 +0800

    Basic Menu Structure

---
+++ Classes/SettingLayer.h
@@ -0,0 +1,33 @@
+//
+//  SettingLayer.h
+//  BattleField_RTS
+//
+//  Created by KHAKhazeus on 2018/5/26.
+//
+
+#ifndef __SETTING_LAYER_H__
+#define __SETTING_LAYER_H__
+
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+class SettingLayer : public cocos2d::Layer
+{
+private:
+    float _node_height;
+    float _node_width;
+    cocos2d::EventListenerTouchOneByOne* _swallow_listner;
+public:
+    
+    virtual bool init() override;
+    
+    virtual void onExit() override;
+    
+    virtual void onEnter() override;
+    
+    void close(cocos2d::Node* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(SettingLayer);
+};
+
+#endif // __SETTING_NODE_H__
